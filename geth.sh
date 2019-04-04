4fdc8f3e3f0c2ae6fb497dd6696cf0a3f7218fab
uriha421 <uriha421@gmail.com>
2019-04-04 17:07:16 +0900

    update geth.sh, see /Users/user2/Work/sample-smart-contract

--- geth.sh
+++ geth.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-geth  --rpc --rpcport 8545 --rpcapi "web3,eth,net,personal" --rpccorsdomain "*" --rpcaddr "0.0.0.0" --datadir "~/eth_private_net" --nodiscover --networkid 15 console 2>> ~/eth_private_net/geth_err.log
+geth  --rpc --rpcport 8545 --rpcapi "web3,eth,net,personal" --rpccorsdomain "*" --rpcaddr "0.0.0.0" --datadir "$PWD/eth_private_net" --nodiscover --networkid 15 console 2>> $PWD/eth_private_net/geth_err.log
